3e8bbe350d511625a737dbd5790e3cb08a51dc4b
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-06-30 11:05:22 -0700

    sticky navbar

--- Content Placeholder/main.js
+++ Content Placeholder/main.js
@@ -12,11 +12,11 @@ setTimeout(getData, 2500)
 
 function getData() {
     header.innerHTML ='<img src="https://blog.richersounds.com/wp-content/uploads/2019/03/image-1024x576.jpeg" alt="">'
-    title.innerHTML = 'Lorem ipsum dolor sit amet.'
-    excerpt.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, eos.'
+    title.innerHTML = 'Bounty hunter and Demon killer'
+    excerpt.innerHTML = 'Name your job and if its intresting enough ill take it, even better if it has demons!'
     profile_img.innerHTML = '<img src="https://assetsio.gnwcdn.com/devil-may-cry-5-najlepsze-umiejtnoci-na-pocztek-nero-v-dante-1553366284550.jpg?width=45&height=45&fit=crop&quality=100&format=png&enable=upscale&auto=webp" alt="">'
-    name.innerHTML = 'Dante'
-    date.innerHTML = 'Oct 08, 2020'
+    name.innerHTML = 'Dante Son of Sparda'
+    date.innerHTML = 'Oct 27, 1973'
 
     animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
     animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
