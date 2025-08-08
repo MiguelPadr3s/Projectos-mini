b60557d64f413d1cdf9b553204a27f5a2334ea85
MiguelPadr3s <Miguelpadresm@hotmail.com>
2025-08-07 17:33:29 -0700

    li transaction

--- Expense Tracker/main.js
+++ Expense Tracker/main.js
@@ -1,6 +1,6 @@
 const balanceEl = document.getElementById("balance");
-const incomeEl = document.getElementById("income-amount");
-const expenseEl = document.getElementById("expense-amount");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
 const transactionListEl = document.getElementById("transaction-list");
 const transactionFormEl = document.getElementById("transaction-form");
 const descriptionEl = document.getElementById("description");
@@ -11,11 +11,48 @@ let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 transactionFormEl.addEventListener("submit", addTransaction);
 
 function addTransaction(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // get form values
-    const description = descriptionEl.value.trim();
-    const amount = amountEl.value;
+  // get form values
+  const description = descriptionEl.value.trim();
+  const amount = parseFloat(amountEl.value);
 
-    console.log(typeof amount);
+  transactions.push({
+    id: Date.now(),
+    description,
+    amount,
+  });
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
+  updateTransactionList();
+  updateSummary();
+
+  transactionFormEl.reset();
+}
+
+function updateTransactionList() {
+  transactionListEl.innerHTML = "";
+
+  const sortedTransactions = [...transactions].reverse();
+
+  sortedTransactions.forEach((transaction) => {
+    const transactionEl = createTransactionElement(transaction);
+    transactionListEl.appendChild(transactionEl);
+  });
+}
+
+function createTransactionElement(transaction) {
+    const li = document.createElement("li")
+    li.classList.add("transaction");
+    li.classList.add(transaction.amount > 0 ? "income" : "expense");
+
+    li.innerHTML = `
+        <span>${transaction.description}</span>
+        <span>${transaction.amount}
+            <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>
+        </span>
+    `;
+
+    return li;
 }
